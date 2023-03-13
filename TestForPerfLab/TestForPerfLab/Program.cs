d5952483d3a7426bf6cc906da139917f6491605b
ArtemYarovoy18 <127687169+ArtemYarovoy18@users.noreply.github.com>
2023-03-13 09:46:13 +0400

    1
    1

--- TestForPerfLab/TestForPerfLab/Program.cs
+++ TestForPerfLab/TestForPerfLab/Program.cs
@@ -2,70 +2,72 @@
 using System.Collections;
 using System.Collections.Generic;
 
-string comand = Console.ReadLine();
-string[] comandMas= comand.Split(' ');
 Random rnd = new Random();
 List<List<string>> listKartIgrokov = new List<List<string>>();
-List<string> kartiIgroka = new List<string>();
 
-switch (comandMas[0])
+while (true)
 {
-    case "start":   //start N C где N-коль-во карт C-коль-во игроков
-        //подготовка колоды
-        var koloda = new List<string>();
-        var cvet = new List<string> {"R","G","B","W"};
-        var a = 0;
-        var nomKart = 0;
-        foreach (var element in cvet)
-        {
-            for (int i = 1; i <= 10; i++)
+    string comand = Console.ReadLine();
+    string[] comandMas = comand.Split(' ');
+    switch (comandMas[0])
+    {
+        case "start":   //start N C где N-коль-во карт C-коль-во игроков
+                        //подготовка колоды
+            var koloda = new List<string>();
+            var cvet = new List<string> { "R", "G", "B", "W" };
+            var a = 0;
+            var nomKart = 0;
+            listKartIgrokov.Clear();
+            foreach (var element in cvet)
             {
-                koloda.Add(element + i);
-                a++;
-            }
-        }
-      koloda.ForEach(Console.WriteLine);
-
-
-
-        if (Convert.ToInt32(comandMas[1])* Convert.ToInt32(comandMas[2]) > koloda.Count)
-        { 
-            Console.WriteLine("колоды в 40 карт нехватает. Введите корректную команду!!!");
-            break;
-        }
-        else
-        {
-
-            for (int i = 0; i < Convert.ToInt32(comandMas[2]); i++) //по игрокам
-            {
-                for (int j = 0; j < Convert.ToInt32(comandMas[1]); j++)//раздаю карты
+                for (int i = 1; i <= 10; i++)
                 {
-                    nomKart = rnd.Next(koloda.Count);
-                    kartiIgroka.Add(koloda[nomKart]);
-                    koloda.RemoveAt(nomKart);
+                    koloda.Add(element + i);
+                    a++;
                 }
-                
-                
-                
-                listKartIgrokov.Add(kartiIgroka);
-              //  kartiIgroka.ForEach(Console.Write);
-                //Console.WriteLine();
-                kartiIgroka.Clear();
             }
-            Console.WriteLine("sfgergweeg   "+listKartIgrokov.Count);
-            Console.WriteLine("sfgergweqfveeg   " + listKartIgrokov[1]);
-
-            foreach (var list in listKartIgrokov)
+            // koloda.ForEach(Console.WriteLine);
+            if (Convert.ToInt32(comandMas[1]) * Convert.ToInt32(comandMas[2]) > koloda.Count)
             {
-                Console.WriteLine(String.Join(", ", list));
+                Console.WriteLine("колоды в 40 карт нехватает. Введите корректную команду!!!");
+                break;
             }
-        }
-        break;
+            else
+            {
+                for (int с = 0; с < Convert.ToInt32(comandMas[2]); с++) //по игрокам
+                {
+                    List<string> kartiIgroka = new List<string>();
+                    for (int n = 0; n < Convert.ToInt32(comandMas[1]); n++)//раздаю карты
+                    {
+                        nomKart = rnd.Next(koloda.Count);
+                        kartiIgroka.Add(koloda[nomKart]);
+                        koloda.RemoveAt(nomKart);
+                    }
+                    listKartIgrokov.Add(kartiIgroka);
+                }
+                /*                foreach (var list in listKartIgrokov)
+                                {
+                                    Console.WriteLine(String.Join(", ", list));
+                                } */
+            }
+            break;
 
+        case "get-cards":
+            if (listKartIgrokov.Count == 0)
+            {
+                Console.WriteLine("Необходимо раздать карты!!!!");
+                break;
+            }
 
-
-
-    case "get-cards":
-        Console.WriteLine("Ваше имя - Tom");
-        break;
+            if (Convert.ToInt32(comandMas[1]) > listKartIgrokov.Count)
+            {
+                Console.WriteLine("Нет такого игрока");
+                break;
+            }
+            else
+            {
+                Console.WriteLine(comandMas[1] + " " + String.Join(" ", listKartIgrokov[Convert.ToInt32(comandMas[1]) - 1]));
+            }
+            break;
+    }
 }
